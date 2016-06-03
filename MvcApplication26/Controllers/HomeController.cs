9f2384465e54860e8b8529d71f95363dbb2efe1a
Avrumi Friedman <info@lakewoodprogramming.com>
2016-06-02 20:38:10 -0400

    Initial commit

---
+++ MvcApplication26/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication26.Controllers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class RandomNumber
+    {
+        public RandomNumber()
+        {
+            People = new List<Person>();
+            People.Add(new Person { Name = "Avrumi", Age = 34 });
+            People.Add(new Person { Name = "John", Age = 45 });
+        }
+        public int Number { get; set; }
+        public int[] Nums { get; set; }
+        public List<Person> People { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetRandom(int min, int max)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(min, max);
+            RandomNumber randomNum = new RandomNumber();
+            randomNum.Number = num;
+            int[] nums = new int[rnd.Next(10, 20)];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(min, max);
+            }
+            randomNum.Nums = nums;
+            return Json(randomNum, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CheckUsername(string username)
+        {
+            var obj = new { isGood = username.Length == 5};
+
+            return Json(obj, JsonRequestBehavior.AllowGet);
+
+
+        }
+    }
+}
---
+++ MvcApplication26/Scripts/index.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+    $("#myButton").on('click', function () {
+        var min = $("#min").val();
+        var max = $("#max").val();
+
+        $.get("/home/getrandom", { min: min, max: max } , function (result) {
+            $("#rand").text(result.Number);
+            result.Nums.forEach(function(num) {
+                $("ul").append("<li>" + num + "</li>");
+            });
+        });
+    });
+
+    $("#username").on('input', function() {
+        var name = $(this).val();
+        $.get('/home/checkusername', { username: name }, function(result) {
+            $("#message").text(result.isGood ? "All good!" : "Sorry, try again");
+            $("#sign-up").prop('disabled', !result.isGood);
+        });
+    });
+
+
+});
